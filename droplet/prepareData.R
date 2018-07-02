7024b59fce268ff762bd51143954bf43247f4fc8
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-02 10:15:29 +0100

    Switched droplet analyses to use BiocFileCache.

--- droplet/prepareData.R
+++ droplet/prepareData.R
@@ -2,11 +2,17 @@ library(scran)
 library(scater)
 library(DropletUtils)
 
+library(BiocFileCache)
+bfc <- BiocFileCache(ask=FALSE)    
+
 ##########################################
 ##########################################
 
 # Pre-processing the 68K PBMC dataset.
-sce.68 <- read10xCounts("raw_data/pbmc68k/hg19/") 
+path.68 <- bfcrpath(bfc, "http://cf.10xgenomics.com/samples/cell-exp/1.1.0/fresh_68k_pbmc_donor_a/fresh_68k_pbmc_donor_a_filtered_gene_bc_matrices.tar.gz")
+tmp.68 <- tempfile()
+untar(path.68, exdir=tmp.68)
+sce.68 <- read10xCounts(file.path(tmp.68, "filtered_matrices_mex/hg19/")) 
 
 library(org.Hs.eg.db)
 symb <- mapIds(org.Hs.eg.db, keys=rownames(sce.68), keytype="ENSEMBL", column="SYMBOL")
@@ -52,7 +58,11 @@ gc()
 ##########################################
 
 # Pre-processing the 4K T-cell dataset.
-sce.4 <- read10xCounts("raw_data/t4k/GRCh38") 
+path.4 <- bfcrpath(bfc, "http://cf.10xgenomics.com/samples/cell-exp/2.1.0/t_4k/t_4k_filtered_gene_bc_matrices.tar.gz")
+tmp.4 <- tempfile()
+untar(path.4, exdir=tmp.4)
+sce.4 <- read10xCounts(file.path(tmp.4, "filtered_gene_bc_matrices/GRCh38/"))
+
 symb <- mapIds(org.Hs.eg.db, keys=rownames(sce.4), keytype="ENSEMBL", column="SYMBOL")
 rowData(sce.4)$Symbol <- symb
 
